000558cfe4fa9ca4c2dfdafb4f17df2c133c0444
Maarten <maartenfabre@gmail.com>
2017-08-01 22:50:01 +0200

    changed whitespace from tab to spaces

--- pdf_concat.py
+++ pdf_concat.py
@@ -9,10 +9,10 @@ output_name = 'kaartjes.pdf'
 merger = PdfFileMerger()
 
 with open(input1_name, 'rb')as input1, \
-	open(input2_name, 'rb')as input2, \
-	open(input3_name, 'rb')as input3, \
-	open(output_name, 'wb') as output:
-	merger.append(fileobj = input1)
-	merger.append(fileobj = input2)
-	merger.append(fileobj = input3)
-	merger.write(output)
+    open(input2_name, 'rb')as input2, \
+    open(input3_name, 'rb')as input3, \
+    open(output_name, 'wb') as output:
+    merger.append(fileobj = input1)
+    merger.append(fileobj = input2)
+    merger.append(fileobj = input3)
+    merger.write(output)
